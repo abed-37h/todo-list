ca85198bc68ebf90b89782ba84fc0d3bebb64cff
Abdurrahman Al-Hariri <79279228+abed-37h@users.noreply.github.com>
2026-03-22 18:53:24 +0200

    Initial commit

---
+++ webpack.dev.js
@@ -0,0 +1,14 @@
+import merge from 'webpack-merge';
+import common from './webpack.common';
+
+export default merge(
+    common,
+        {
+        mode: 'development',
+        devtool: 'eval-source-map',
+        devServer: {
+            watchFiles: ['./src/index.html'],
+        },
+    },
+);
+
