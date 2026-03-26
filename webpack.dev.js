59499c3c2796789ac5f8a666b50403a5324782ff
abed-37h <79279228+abed-37h@users.noreply.github.com>
2026-03-26 16:57:21 +0200

    Fix errors in webpack template

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,9 +1,9 @@
-import merge from 'webpack-merge';
-import common from './webpack.common';
+import { merge } from 'webpack-merge';
+import common from './webpack.common.js';
 
 export default merge(
     common,
-        {
+    {
         mode: 'development',
         devtool: 'eval-source-map',
         devServer: {
